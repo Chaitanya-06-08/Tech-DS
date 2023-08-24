24a02d12c11326d93513c4c1ad7f5fb6fd6cf158
Chaitanya <chaitanyadhavaleswarapu@gmail.com>
2023-08-24 18:38:20 +0530

    Modified operations file in modulus branch

--- Operations.java
+++ Operations.java
@@ -13,6 +13,7 @@ class Operations
 		System.out.println(a-b);
 		System.out.println(a*b);
 		System.out.println(a/b);
+		System.out.println(a%b);
 		
 	}
 	public static void main (String[] args) throws java.lang.Exception
@@ -21,6 +22,7 @@ class Operations
 		operation("-",1,2);
 		operation("*",1,2);
 		operation("/",1,2);
+		operation("%",1,2);
 		// your code goes here
 	}
 }
